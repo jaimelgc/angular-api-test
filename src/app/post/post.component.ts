c83cab62efcef88bcb657532c1c338d81b387d5d
jaimelgc <jaimeleongarcia123@gmail.com>
2024-03-05 12:02:10 +0000

    load posts service

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { PostService } from '../services/post.service';
 
 @Component({
   selector: 'app-post',
@@ -9,18 +10,24 @@ import { Component, inject, OnInit } from '@angular/core';
   styleUrl: './post.component.css'
 })
 export class PostComponent implements OnInit {
-  http = inject(HttpClient);
+  // http = inject(HttpClient);
+  private postService = inject(PostService);
   posts: any = [];
 
   ngOnInit(): void {
-    this.fetchPosts();
+    this.loadPosts();
   }
-  fetchPosts(){
-    this.http.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
-    .subscribe((posts: any) => {
+  // fetchPosts(){
+  //   this.http.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+  //   .subscribe((posts: any) => {
+  //     console.log(posts);
+  //     this.posts = posts;
+  //   });
+  // }
+  loadPosts() {
+    this.postService.getPosts().subscribe((posts: any) => {
       console.log(posts);
-      this.posts = posts;
+      this.posts = posts
     });
-    
   }
 }
